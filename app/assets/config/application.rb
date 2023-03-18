0f07561a5e1c17a59402aaa516d64fcdf9032a7d
takashi-kinoshita <121014620+takashi-kinoshita@users.noreply.github.com>
2023-03-19 08:07:15 +0900

    newapp1

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -1,12 +1,10 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Myapp
+module YourAppName
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -16,9 +14,7 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-    config.action_controller.permit_all_parameters = true
-    config.time_zone = 'Tokyo'  
+    # config.time_zone = 'Central Time (US & Canada)'
+    # config.eager_load_paths << Rails.root.join('extras')
   end
 end
